1aca504ca71b6d4c9566ff26b450dd76e2064e72
bookug <zengli@bookug.cc>
2018-11-12 16:34:28 +0800

    docs: change scripts

--- script/run.sh
+++ script/run.sh
@@ -5,7 +5,6 @@
 #make clean
 #make
 
-# NOTICE: make sure that each dataset has exactly 100 queries
 data=/home/data/DATASET/
 
 # To run delaunay_n13
@@ -20,7 +19,8 @@ do
     ./GunrockSM.exe /home/data/DATASET/delaunay_n13/delaunay_n13.g ${query} ${result1}${file%.*}.txt 2 >& ${result1}${file%.*}.log
     grep "match used: " ${result1}${file%.*}.log >> ${file1}
 done
-awk 'BEGIN{t=0.0}{t = t + $3}END{printf("the average time of answering delaunay_n13 queries: %.2f ms\n", t/100);}' ${file1}
+# TO find out which one fails, using grep -r "match used: " delaunay_n13.log
+awk 'BEGIN{t=0.0;cnt=0}{t = t + $3;cnt=cnt+1}END{printf("the average time of answering delaunay_n13 queries: %.2f ms\n", t/cnt);}' ${file1}
 /bin/rm -f $file1
 echo "delaunay_n13 ends"
 
@@ -35,7 +35,7 @@ do
     ./GunrockSM.exe /home/data/DATASET/enron/enron.g ${query} ${result2}${file%.*}.txt 2 >& ${result2}${file%.*}.log
     grep "match used: " ${result2}${file%.*}.log >> ${file2}
 done
-awk 'BEGIN{t=0.0}{t = t + $3}END{printf("the average time of answering enron queries: %.2f ms\n", t/100);}' ${file2}
+awk 'BEGIN{t=0.0;cnt=0}{t = t + $3;cnt=cnt+1}END{printf("the average time of answering enron queries: %.2f ms\n", t/cnt);}' ${file2}
 /bin/rm -f $file2
 echo "enron ends"
 
@@ -50,7 +50,7 @@ do
     ./GunrockSM.exe /home/data/DATASET/gowalla/loc-gowalla_edges.g ${query} ${result3}${file%.*}.txt 2 >& ${result3}${file%.*}.log
     grep "match used: " ${result3}${file%.*}.log >> ${file3}
 done
-awk 'BEGIN{t=0.0}{t = t + $3}END{printf("the average time of answering enron queries: %.2f ms\n", t/100);}' ${file3}
+awk 'BEGIN{t=0.0;cnt=0}{t = t + $3;cnt=cnt+1}END{printf("the average time of answering gowalla queries: %.2f ms\n", t/cnt);}' ${file3}
 /bin/rm -f $file3
 echo "gowalla ends"
 
@@ -65,7 +65,7 @@ do
     ./GunrockSM.exe /home/data/DATASET/road_central/road_central.g ${query} ${result4}${file%.*}.txt 2 >& ${result4}${file%.*}.log
     grep "match used: " ${result4}${file%.*}.log >> ${file4}
 done
-awk 'BEGIN{t=0.0}{t = t + $3}END{printf("the average time of answering enron queries: %.2f ms\n", t/100);}' ${file4}
+awk 'BEGIN{t=0.0;cnt=0}{t = t + $3;cnt=cnt+1}END{printf("the average time of answering gowalla queries: %.2f ms\n", t/cnt);}' ${file4}
 /bin/rm -f $file4
 echo "road_central ends"
 
